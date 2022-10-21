d93c36e82ecb8c6a618e44020296a74502918287
suran-kim <suran221@gmail.com>
2022-10-21 23:07:47 +0900

    feat: 입력 페이지

--- src/main/java/com/example/im_fruit/controller/ProductController.java
+++ src/main/java/com/example/im_fruit/controller/ProductController.java
@@ -1,11 +1,13 @@
 package com.example.im_fruit.controller;
 
 import com.example.im_fruit.service.ProductService;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class ProductController {
-
     private final ProductService productService;
 
     public ProductController(ProductService productService) {
@@ -17,5 +19,19 @@ public class ProductController {
         var products = productService.getAllProducts();
         model.addAttribute("products", products);
         return "product-list";
+    }
+
+    @GetMapping("new-product")
+    public String newProductPage() {
+        return "new-product";
+    }
+
+    @PostMapping("/products")
+    public String newProduct(CreateProductRequest createProductRequest) {
+        productService.createProduct(createProductRequest.productName(),
+                createProductRequest.category(),
+                createProductRequest.price(),
+                createProductRequest.description());
+        return "redirect:/products";
     }
 }
