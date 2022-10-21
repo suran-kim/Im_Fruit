6d0f86b6b45c74ea8a8d5c2d7b86d0b7372fe60e
suran-kim <suran221@gmail.com>
2022-10-21 18:42:05 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/example/im_fruit/ImFruitApplication.java
@@ -0,0 +1,13 @@
+package com.example.im_fruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImFruitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImFruitApplication.class, args);
+    }
+
+}
